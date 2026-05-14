ee26db5f9840ecf64266e63c143778dd0cce6548
Renan Venturini <renanventurinioficial@gmail.com>
2026-05-14 10:13:45 -0300

    Projeto inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </header>
+      <section>
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </section>
+    </main>
+  )
+}
+
+export default App
